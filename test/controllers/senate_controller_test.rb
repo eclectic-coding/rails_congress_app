630355040827907d689d289ae0538bdcde202126
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-05-05 15:09:40 -0400

    Remove senate controller

--- test/controllers/senate_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SenateControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get senate_index_url
-    assert_response :success
-  end
-
-end
