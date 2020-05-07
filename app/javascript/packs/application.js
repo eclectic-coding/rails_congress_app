2acdb3a6bb61f64f64bea98a1cce287f32a503ed
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-05-07 19:00:55 -0400

    Implemented Tailwind CSS ... whee

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// require("jquery")
+require("../stylesheets/application.scss")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -16,4 +18,25 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+
 console.log("hello from application.js")
+
+// Navbar Toggle
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+                // Get the "main-nav" element
+                var $target = document.getElementById('main-nav');
+                // Toggle the class on "main-nav"
+                $target.classList.toggle('hidden');
+            });
+        });
+    }
+});
+
+
