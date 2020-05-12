426fa98cda9c7b4711e2d2aa4df84816901bee7b
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-05-11 21:56:01 -0400

    Finished Stimulus dropdown tailwind menu4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,33 +10,19 @@ require("channels")
 // require("jquery")
 require("../stylesheets/application.scss")
 
+// Start StimulusJS
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const application = Application.start();
+const context = require.context("controllers", true, /.js$/);
+application.load(definitionsFromContext(context));
 
-
-console.log("hello from application.js")
-
-// Navbar Toggle
-document.addEventListener('DOMContentLoaded', function () {
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-        // Add a click event on each of them
-        $navbarBurgers.forEach(function ($el) {
-            $el.addEventListener('click', function () {
-                // Get the "main-nav" element
-                var $target = document.getElementById('main-nav');
-                // Toggle the class on "main-nav"
-                $target.classList.toggle('hidden');
-            });
-        });
-    }
-});
-
+// Import and register all TailwindCSS Components
+import { Dropdown } from "tailwindcss-stimulus-components"
+application.register('dropdown', Dropdown)
+// application.register('modal', Modal)
+// application.register('tabs', Tabs)
+// application.register('popover', Popover)
+// application.register('toggle', Toggle)
 
