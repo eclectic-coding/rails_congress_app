7838965469195c7e77ccf2b9043dce711a302c09
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-05-28 16:51:03 -0400

    Setup production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'yourcongress.co' }
+  host = 'salty-forest-30498.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
@@ -9,8 +10,8 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
-    user_name: 'apikey',
-    password: Rails.application.credentials.dig(:SENDGRID_API_KEY)
+    user_name: Rails.application.credentials.dig(:sendgrid_username),
+    password: Rails.application.credentials.dig(:sendgrid_password)
     # enable_starttls_auto: true
   }
   # Eager load code on boot. This eager loads most of Rails and
