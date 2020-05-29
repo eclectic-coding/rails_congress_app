2d063b0181073f95f580d5014fb30d0bf42bf195
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-05-29 07:18:03 -0400

    Tweak API keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,13 +6,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
+    user_name: 'apikey',
+    password: Rails.application.credentials[:sendgrid_api_key]
     domain: 'yourcongress.co',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
-    user_name: 'apikey',
-    password: Rails.application.credentials.dig(:sendgrid_api_key)
-    # enable_starttls_auto: true
+    enable_starttls_auto: true
   }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
