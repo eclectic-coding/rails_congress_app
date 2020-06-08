3d3cc5e9c9a1b3e03ca0b28ba4c07e74b897655f
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-06-08 08:56:44 -0400

    Fix PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -16,7 +16,7 @@ let environment = {
 if (process.env.RAILS_ENV === "production") {
   environment.plugins.push(
       require('@fullhuman/postcss-purgecss')({
-        content: ['./app/**/*.html.erb', './app/helpers/**/*.rb'],
+        content: ['./app/**/*.html.haml', './app/helpers/**/*.rb'],
         defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
       })
   )
