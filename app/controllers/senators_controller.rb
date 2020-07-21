561385c100168f222cd3e9c579dab1a760cb9234
eclecticcoding <chuck@eclecticsaddlebag.com>
2020-07-21 07:21:55 -0400

    # If applied, this commit will...
    # Why is this change needed?
    
    # How does it address the issue?
    
    # Provide links to any relevant tickets, articles or other resources
    refactor controllers

--- app/controllers/senators_controller.rb
+++ app/controllers/senators_controller.rb
@@ -5,12 +5,10 @@ class SenatorsController < ApplicationController
     @senators = Member.where(chamber: 'senate')
                       .order(last_name: :asc)
                       .paginate(page: params[:page], per_page: 20)
-    @senators_all = Member.where(chamber: 'senate')
   end
 
   def show
     @senator = Member.find_by(params[member_id: :id])
-    byebug
   end
 
 end
