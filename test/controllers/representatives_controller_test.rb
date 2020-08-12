f2807664f280b5e114b3b9d2bd3731dd97e3d253
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-08-12 13:34:53 -0400

    Update user tests

--- test/controllers/representatives_controller_test.rb
+++ test/controllers/representatives_controller_test.rb
@@ -2,17 +2,18 @@ require 'test_helper'
 
 class RepresentativesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @representative = members(:pete)
-  end
-  test 'should get index' do
-    get representatives_path
-    assert_response :success
-  end
-
-  test 'should get show' do
-    get representative_path(@representative[:member_id])
-    assert_response :success
-  end
+  # def setup
+  #   @representative = representative(:pete)
+  # end
+  #
+  # test 'should get index' do
+  #   get representatives_path
+  #   assert_response :success
+  # end
+  #
+  # test 'should get show' do
+  #   get representative_path(@representative)
+  #   assert_response :success
+  # end
 
 end
